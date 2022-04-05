772ad8e413c7115af720bcdb692de076c7ae3b5d
mpwow5 <mpwow5@gmail.com>
2022-04-05 12:30:27 +0300

    Removed comments

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from flask import request
 
 
 class Main(Resource):
-    """Метод GET. В запросе указываем параметр episodes_id.
-    episodes_id = 0 возвращает список всех эпизодов.
-    Если указан отсутствущий episode_id - возвращается информационное сообщение"""
 
     def get(self):
         episode_id = request.args.get('episode_id', type=int)
@@ -18,8 +15,6 @@ class Main(Resource):
         else:
             return episodes[episode_id]
 
-    """Метод удаляет указанный в параметрах запроса эпизод"""
-
     def delete(self):
         episode_id = request.json['episode_id']
         if episode_id not in episodes.keys():
@@ -27,8 +22,6 @@ class Main(Resource):
         else:
             del episodes[episode_id]
             return {"message": "Episode successfuly deleted"}
-
-    """Метод позволяет добавлять эпизоды"""
 
     def post(self):
         content_type = request.headers.get('Content-Type')
@@ -40,8 +33,6 @@ class Main(Resource):
         else:
             abort(415, message="Unsupported Media Type")
 
-    """Изменяем описания уже добавленных эпизодов"""
-
     def put(self):
         episode_id = request.args.get('episode_id', type=int)
         content_type = request.headers.get('Content-Type')
@@ -50,9 +41,7 @@ class Main(Resource):
                 abort(404, message="This episode doesn't exist")
             else:
                 json = request.json
-                #  В переданных параметрах получаем json. Находим эпизод по episode_id и перебираем ключи в полученном
-                #  json и сверяем с ключами в словаре эпизода. Если есть совпадения, перезаписываем
-                #  старые значения значениями из полученного json
+
                 for key in json:
                     for key2 in episodes[episode_id]:
                         if key == key2:
